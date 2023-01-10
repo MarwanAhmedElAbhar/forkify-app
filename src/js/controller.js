127598ef49a4f3326868a5a7c7d421bfaa9c5124
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-01-11 00:38:05 +0200

    add controller js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,8 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import * as model from "./model"
+import recipeView from './views/recipeView';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +16,29 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1)
+
+    if (!id) return;
+
+    recipeView.renderSpinner()
+
+    await model.loadRecipe(id)
+
+    recipeView.render(model.state.recipe)
+
+
+  } catch (err) {
+    alert(err)
+  }
+}
+
+
+// ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
+
+window.addEventListener('hashchange', controlRecipes)
+window.addEventListener('load', controlRecipes)
