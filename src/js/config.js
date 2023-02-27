e24458285eb2a1bf089cfbb82bfd7e735832406b
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-02-27 11:44:08 +0200

    add API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes"
 export const TIMEOUT_SEC = 10
 export const RES_PER_PAGE = 10
+export const KEY = "066556f9-bd31-4036-b53a-4935d3fe7f4b"
